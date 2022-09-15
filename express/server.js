cda1c1bfb7c98d77c1cc6f3c933068c9bce13a91
qinghua644946965 <78455113+qinghua644946965@users.noreply.github.com>
2022-09-15 21:04:06 +0800

    sdf
    sadafsad

--- express/server.js
+++ express/server.js
@@ -12,6 +12,24 @@ router.get('/', (req, res) => {
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get('/weather/:city', (req, res) => {
+  const city = req.params.city;
+  console.log("req.params.city",city);
+  if(city){
+    console.log("encodeURI city",encodeURIComponent(city));
+    var options = {
+      method: "POST",
+      url: "https://way.jd.com/jisuapi/weather?city="+encodeURIComponent(city)+"&appkey=1249e6ecf8b79c0c403105a14b0dc36e",
+      headers: {},
+    };
+    request(options, function (error, response) {
+      if (error) throw new Error(error);
+      res.send(response.body);
+    });
+  }else{
+    res.send("未设置参数city");
+  }
+});
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
