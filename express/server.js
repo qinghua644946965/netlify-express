eadddd6c1e8ce97a08fa8952722c2db99338a845
hua-macbook <ranguoxing456@gmail.com>
2022-09-15 19:26:52 +0800

    main

---
+++ express/server.js
@@ -0,0 +1,38 @@
+'use strict';
+const express = require('express');
+var cors = require('cors')
+var request = require("request");
+const path = require('path');
+const serverless = require('serverless-http');
+const app = express();
+app.use(cors())
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+router.get('/', (req, res) => {
+    const city = req.query.city;
+    console.log("city",city);
+    if(city){
+      console.log("encodeURI city",encodeURIComponent(city));
+      var options = {
+        method: "POST",
+        url: "https://way.jd.com/jisuapi/weather?city="+encodeURIComponent(city)+"&appkey=1249e6ecf8b79c0c403105a14b0dc36e",
+        headers: {},
+      };
+      request(options, function (error, response) {
+        if (error) throw new Error(error);
+        res.send(response.body);
+      });
+    }else{
+      res.send("未设置参数city");
+    }
+});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
