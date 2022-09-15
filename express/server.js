9f16da665d3f737e0475a297d3197161628709a7
hua-macbook <ranguoxing456@gmail.com>
2022-09-15 20:33:45 +0800

    Update server.js

--- express/server.js
+++ express/server.js
@@ -36,7 +36,6 @@ router.get('/weather/:city', (req, res) => {
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
