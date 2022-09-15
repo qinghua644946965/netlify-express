6d2b0260b184370a0b0f664c0335322eb443b027
hua-macbook <ranguoxing456@gmail.com>
2022-09-15 19:53:06 +0800

    Update server.js

--- express/server.js
+++ express/server.js
@@ -10,22 +10,25 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-    const city = req.params.city;
-    console.log("req.params.city",city);
-    if(city){
-      console.log("encodeURI city",encodeURIComponent(city));
-      var options = {
-        method: "POST",
-        url: "https://way.jd.com/jisuapi/weather?city="+encodeURIComponent(city)+"&appkey=1249e6ecf8b79c0c403105a14b0dc36e",
-        headers: {},
-      };
-      request(options, function (error, response) {
-        if (error) throw new Error(error);
-        res.send(response.body);
-      });
-    }else{
-      res.send("未设置参数city");
-    }
+  res.send('Serverless Node.js Express!')
+});
+router.get('/weather/:city', (req, res) => {
+  const city = req.params.city;
+  console.log("req.params.city",city);
+  if(city){
+    console.log("encodeURI city",encodeURIComponent(city));
+    var options = {
+      method: "POST",
+      url: "https://way.jd.com/jisuapi/weather?city="+encodeURIComponent(city)+"&appkey=1249e6ecf8b79c0c403105a14b0dc36e",
+      headers: {},
+    };
+    request(options, function (error, response) {
+      if (error) throw new Error(error);
+      res.send(response.body);
+    });
+  }else{
+    res.send("未设置参数city");
+  }
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
