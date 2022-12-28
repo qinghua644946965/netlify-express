0c9b33fc6a8a4080fd6194a2af4605440dca53c1
qinghua644946965 <78455113+qinghua644946965@users.noreply.github.com>
2022-12-28 19:01:28 +0800

    tests

--- express/server.js
+++ express/server.js
@@ -15,23 +15,17 @@ router.get('/', (req, res) => {
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
-router.get('/weather/:city', (req, res) => {
-  const city = req.params.city;
-  console.log("req.params.city",city);
-  if(city){
-    console.log("encodeURI city",encodeURIComponent(city));
-    var options = {
-      method: "POST",
-      url: "https://way.jd.com/jisuapi/weather?city="+encodeURIComponent(city)+"&appkey=1249e6ecf8b79c0c403105a14b0dc36e",
-      headers: {},
-    };
-    request(options, function (error, response) {
-      if (error) throw new Error(error);
-      res.send(response.body);
+router.get('/set', (req, res) => {
+    var params = req.params;
+    console.log("req.params",params);
+    Object.keys(params).map(key=>{
+        ds[key] = params.key
     });
-  }else{
-    res.send("未设置参数city");
-  }
+});
+
+router.get('/get/:key', (req, res) => {
+    var key = req.params.key;
+    res.send(ds[key]);
 });
 
 app.use(bodyParser.json());
